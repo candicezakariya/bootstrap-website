2078ca0a282c299dac50331b94759fedc6c170b3
candicezakariya <candicelzakariya@gmail.com>
2023-02-25 12:54:58 -0600

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,12 @@ $(function () {
       $(".carousel").carousel("cycle");
     });
   });
+
+$(function () {
+    $("#reserveButton").click(function(){
+      $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+      $("#loginModal").modal("show");
+    });
+});
